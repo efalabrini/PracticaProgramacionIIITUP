f529a94b491766e0caa693bcc8e36be18fc0ae1e
NicoMart <nicomartinez054@gmail.com>
2023-09-09 11:16:58 -0300

    Ej5 Resuelto

---
+++ Practica2/Ej5/Coche .cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica2.Ej5
+{
+    public class Coche : IVehiculo
+    {
+        private bool _Combustible; 
+        public Coche(int combustible) 
+        {
+            _Combustible = Convert.ToBoolean(combustible);
+        }
+
+        public bool CargarCombustible(int cantidad)
+        {
+            if (cantidad > 0)
+            {
+                Console.WriteLine("Cargando gasolina ...");
+                Console.WriteLine("Gasolina cargada con exito.");
+                return _Combustible = true;
+            }
+            else
+            {
+                Console.WriteLine("La cantidad de gasolina a cargar debe ser mayor que 0.");
+                return _Combustible = false;
+            }
+        }
+
+        public void Conducir()
+        {
+            if( _Combustible == true) { Console.WriteLine("Estoy conduciendo"); }
+            else { Console.WriteLine("El coche no tiene gasolina, Yendo a cargar"); }
+            
+        }
+    }
+}
---
+++ Practica2/Ej5/Ej5.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica2.Ej5
+{
+    public class Ej5 
+    {
+        public void Ejecutar()
+        {
+            Coche coche = new Coche(0);
+            coche.Conducir();
+
+            Console.WriteLine("Cuantos Litros de Gasolina desea cargar: ");
+            int carga = int.Parse(Console.ReadLine());
+            coche.CargarCombustible(carga);
+            coche.Conducir();
+        }
+    }
+}
