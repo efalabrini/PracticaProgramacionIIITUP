33c2f3937726f7d3e6dddefba6dc7bb089a5830c
sebatian sueiro <s.sueiro@hotmail.com>
2023-09-10 19:08:12 -0300

    hice la practica

---
+++ Practica2/Ej5/Coche.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica2.Ej5
+{
+    public class Coche : IVehiculo
+    {
+
+        public int cantidadcombustible { get;  set; }
+
+        public  Coche(int CantidadCombustible)
+        {
+            cantidadcombustible = CantidadCombustible;
+        }
+        public void Conducir() 
+        {
+            if (cantidadcombustible > 0) 
+            { 
+                Console.WriteLine("El coche se esta conduciendo!"); 
+            }
+            else 
+            {
+                Console.WriteLine("¡No hay nafta, llename el tanque!"); 
+            }
+        }
+
+        public bool CargarCombustible(int sumaNafta) 
+        {
+            cantidadcombustible += sumaNafta;
+            return true;
+        }
+    }
+}
---
+++ Practica2/Ej5/Ej5.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica2.Ej5
+{
+    public class Ej5
+    {
+        public void Ejecutar()
+        {
+            Console.WriteLine("Cuanta nafta tenes?");
+            int NaftaInicial = int.Parse(Console.ReadLine());
+            Coche FordKa = new(NaftaInicial);
+            Console.WriteLine(FordKa.cantidadcombustible);
+            FordKa.Conducir();
+
+            Console.WriteLine("Cuanta Nafta vas a cargar?");
+            int CantNafta = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(FordKa.CargarCombustible(CantNafta));
+            Console.WriteLine($"Cantidad total de nafta: {FordKa.cantidadcombustible}");
+        }
+    }
+}
