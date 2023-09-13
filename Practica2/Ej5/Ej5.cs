86d6b9e881a8af7036cc5085e63379278fa25268
MoraCapdevila <moracapdevila457@gmail.com>
2023-09-13 14:49:44 -0300

    cambios ej5

---
+++ Practica2/Ej5/Ej5.cs
@@ -0,0 +1,27 @@
+﻿using Practica2.Ej3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica2.Ej5
+{
+    public class Ej5
+    {
+        public void Ejecutar()
+        {
+            // El Coche con 0 de gasolina inicial
+            Coche miCoche = new Coche(0);
+
+
+            Console.Write("Ingrese una cantidad de combustible para cargar: ");
+            int combustible = int.Parse(Console.ReadLine()); //no pude usar int.TryParse pq tenia que hacer un out 
+
+            miCoche.CargarCombustible(combustible);
+
+            miCoche.Conducir();
+
+        }
+    }
+}
---
+++ Practica2/Ej5/IVehiculo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica2.Ej5
+{
+    interface IVehiculo
+    {
+        protected void Conducir(); //debbo dejar los protected o no es necesario?
+
+        protected bool CargarCombustible(int cant_combustible);
+    }
+}
