d4657116f404fcacd022f9bd299d3a1ebec6b43f
IsTincho <segninitincho@gmail.com>
2023-09-13 14:24:06 -0300

    Ej5 Added

---
+++ Practica2/Ej5/Coche.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica2.Ej5
+{
+    internal class Coche : IVehiculo
+    {
+        private int combustible;
+
+        public Coche(int combustibleInicial)
+        {
+            combustible = combustibleInicial;
+        }
+        public void Conducir()
+        {
+            if (combustible > 0)
+            {
+                Console.WriteLine("El coche está conduciendo.");
+            }
+            else
+            {
+                Console.WriteLine("No hay suficiente combustible para conducir");
+            }
+        }
+
+        public bool CargarCombustible(int cantidad)
+        {
+            if (cantidad < 0)
+            {
+                Console.WriteLine("La cantidad de combustible a cargar no puede ser negativa.");
+                return false;
+            }
+
+            combustible += cantidad;
+            Console.WriteLine($"Se han cargado {cantidad} unidades de combustible. Combustible total: {combustible}");
+            return true;
+
+        }
+
+
+    }
+}
