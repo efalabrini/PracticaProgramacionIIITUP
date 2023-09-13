d381831069350e53fd6432a8343c16b824f4372d
matshun1238 <mazonchin@hotmail.com>
2023-09-13 16:04:32 -0300

    Ej5 | Guido Montenegro

---
+++ Practica2/Ej5/Ej5.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica2.Ej5
+{
+    public class Ej5
+    {
+        public void Ejecutar()
+        {
+            Coche miCoche = new Coche(0);
+            miCoche.Conducir();
+            miCoche.CargarCombustible(45);
+            miCoche.Conducir();
+        }
+    }
+}
---
+++ Practica2/Ej5/IVehiculo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica2.Ej5
+{
+    internal interface IVehiculo
+    {
+            public void Conducir();
+
+            public bool CargarCombustible(int cantCombustible);
+    }
+}
