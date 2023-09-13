8cc7b5518507f845f64ad1549b599b7f16041a70
Alvaro Johansen <109972764+Alvarojohansen@users.noreply.github.com>
2023-09-13 15:35:04 -0300

    ejercicio 5 terminado

---
+++ Practica2/Ej5/Coche.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica2.Ej5
+{
+    public class Coche: IVehiculo
+    {
+        private int gasoil;
+
+        public Coche(int gasoilInicial)
+        {
+            gasoil = gasoilInicial;
+        }
+
+        public void Conducir()
+        {
+            if (gasoil > 0)
+            {
+                Console.WriteLine("El coche está conduciendo.");
+            }
+            else
+            {
+                Console.WriteLine("No hay suficiente gasolina para conducir.");
+            }
+        }
+
+        public bool Cargar(int cantidad)
+        {
+            if (cantidad > 0)
+            {
+                gasoil += cantidad;
+                Console.WriteLine($"Se han cargado {cantidad} unidades de gasolina.");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("La cantidad de combustible a cargar debe ser mayor que 0.");
+                return false;
+            }
+        }
+
+        public bool CargarCombustible(int cantidad)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Practica2/Ej5/Ej5.cs
+++ Practica2/Ej5/Ej5.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace Practica2.Ej5
 {
-    internal class Ej5
+    public class Ej5
     {
+        public void Ejecutar()
+        {
+            // Crear un objeto de tipo Coche con 0 de gasolina
+            Coche miCoche = new Coche(0);
+
+            // Solicitar al usuario la cantidad de combustible para cargar
+            Console.Write("Ingrese la cantidad de gasolina a cargar: ");
+            if (int.TryParse(Console.ReadLine(), out int cantidadCarga))
+            {
+                // Llamar al método CargarCombustible
+                if (miCoche.Cargar(cantidadCarga))
+                {
+                    // Ejecutar el método Conducir después de cargar combustible
+                    miCoche.Conducir();
+                }
+            }
+            else
+            {
+                Console.WriteLine("La entrada no es válida. Debe ingresar un número entero.");
+            }
+        }
     }
 }
