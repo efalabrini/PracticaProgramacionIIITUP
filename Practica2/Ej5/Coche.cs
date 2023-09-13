7591529fec0a2f65dffad3019f2cd97f60c3e9c9
TournierSebastian <110041924+TournierSebastian@users.noreply.github.com>
2023-09-13 16:27:13 -0300

    Ejercicio5

---
+++ Practica2/Ej5/Coche.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodaPractica.Practica2.Ej5
+{
+   
+    public class Coche : IVehiculo
+    {
+        public int gasolina;
+        public Coche(int CombustibleInicial)
+        {
+            gasolina = CombustibleInicial;
+        }
+
+        public bool CargarCombustible(int CantCombustible)
+        {
+
+
+            if (CantCombustible > 0) 
+            {
+                gasolina =+ CantCombustible;
+                Console.WriteLine($"se cargo  {CantCombustible}de nafta. El tanque tiene {gasolina}");
+                return true;
+            }else
+            {
+                Console.WriteLine("no se cargo");
+                return false;
+            }
+        }
+
+        public void Conducir()
+        {
+            if (gasolina > 0)
+            {
+                Console.WriteLine("El Coche esta Conduciendo");
+            }
+            else 
+            {
+                Console.WriteLine("No tiene Nafta");
+            }
+        }
+    }
+}
---
+++ Practica2/Ej5/Ej5.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodaPractica.Practica2.Ej5
+{
+    public class Ej5
+    {   
+        public void Ejecutar()
+        {
+            Coche Auto1 = new Coche(0);
+            Auto1.Conducir();
+            Console.WriteLine("Ingrese la cantidad de combustible que desea cargar: ");
+            int Carga = int.Parse(Console.ReadLine());
+
+            Auto1.CargarCombustible(Carga);
+            Auto1.Conducir();
+
+        }
+    }
+}
