31d9dcb989701bfaca540367aab5e9c4b9f7e801
SantiB02 <santibrasca02@gmail.com>
2023-09-13 14:20:32 -0300

    Ejercicio 5

---
+++ Practica2/Ej5/Coche.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica2.Ej5
+{
+    internal class Coche : IVehiculo
+    {
+        public int Gasolina { get; set; }
+        public Coche(int gasolinaInicial)
+        {
+            Gasolina = gasolinaInicial;
+        }
+        public void Conducir()
+        {
+            if (Gasolina > 0)
+            {
+                Console.WriteLine("El coche está Conduciendo");
+            }
+        }
+        public bool CargarCombustible(int combustible)
+        {
+            Gasolina += combustible;
+            return true;
+        }
+    }
+}
