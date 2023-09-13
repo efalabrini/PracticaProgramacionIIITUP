e640d3c9933f1e6e0b6d37d0ded9aec48c3d162c
Lorenzo Carignani <lorenzocarignani@outlook.com>
2023-09-13 13:47:49 -0300

    Exercise 5 done

---
+++ Practica2/Ej5/Coche.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica2.Ej5
+{
+    public class Coche : IVehiculo
+    {
+        private int Gasolina { get; set; }
+
+        public Coche(int gasolina)
+        {
+            Gasolina = gasolina;
+        }
+
+        public void Conducir()
+        {
+            if (Gasolina > 0)
+            {
+                Console.WriteLine("Conduciendo");
+            }
+            else
+            {
+                Console.WriteLine("Necesita combusitble para manejar el coche");
+            }
+        }
+
+        public bool CargarCombustible(int cantidad)
+        {
+            bool band = false;
+
+            do
+            {
+                if (cantidad > 0)
+                {
+                    Gasolina += cantidad;
+                    Console.WriteLine($"Se han cargado {cantidad} unidades de gasolina.");
+                    return band == true;
+                }
+                else
+                {
+                    Console.WriteLine("La cantidad de combustible a cargar debe ser mayor que 0.");
+                    return band == false;
+                }
+
+            } while (cantidad < 0 && band == false);
+        }
+    }
+}
--- Practica2/Program.cs
+++ Practica2/Program.cs
@@ -3,13 +3,29 @@ using Practica2.Ej1;
 using Practica2.Ej2;
 using Practica2.Ej3;
 using Practica2.Ej4;
+using Practica2.Ej5;
 
-Console.WriteLine("Práctica 2 Programación 3 TUP");
-Console.WriteLine("-----------------------------");
+namespace Practica2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
 
-var ej = new Ej4();
+            Coche coche = new Coche(0);
+            Console.WriteLine("Ingrese la cantidad a cargar: ");
+            if (int.TryParse(Console.ReadLine(), out int cantidad))
+            {
+                if (coche.CargarCombustible(cantidad))
+                {
+                    coche.Conducir();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Cantidad ingresada no valida");
+            }
 
-Console.WriteLine($"Ejercicio {ej.GetType().Name}");
-Console.WriteLine();
-
-ej.Ejecutar();
+        }
+    }
+}
