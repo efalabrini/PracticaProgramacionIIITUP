c62ca2aa6deb01f7ffa65fc0ef84a1b7ab2d8b0f
mati herrero <matiherrero09@gmail.com>
2023-09-13 16:15:28 -0300

    Added Ej5

---
+++ Practica2/Ej5/IVehiculo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodaPractica.Practica2.Ej5
+{
+    public interface IVehiculo
+    {
+        public void Conducir();
+        public bool CargarCombustible(int CantCombustible);
+    }
+}
